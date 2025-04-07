8b018ffd35c7bdbcd8f8cfd02273b427028a9919
Arnavghimire <ghimirearnav738@gmail.com>
2025-04-07 21:02:35 +0545

    add it

---
+++ queno.1.py
@@ -0,0 +1,11 @@
+def counts(filename):
+    with open(filename, 'r') as inputfile:
+        files = inputfile.read()
+        count=0
+        for char in files:
+            if char !=' 'and char!='\n':    
+                count+=1
+        print(count)
+        
+       
+counts('file.txt')
